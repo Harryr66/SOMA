52903f0dd9086a0fed19b052ef15172c5c92bedd
Harry Rollerson <hrollerson1@gmail.com>
2025-11-11 11:56:58 +0800

    Bust Gouache logo cache to v2

--- src/components/desktop-header.tsx
+++ src/components/desktop-header.tsx
@@ -36,14 +36,14 @@ export function DesktopHeader() {
       <Link href="/discover" className="flex items-center">
         <span className="sr-only">Gouache</span>
         <img
-          src="/assets/gouache-logo-light.png?v=1"
+          src="/assets/gouache-logo-light.png?v=2"
           alt="Gouache"
           width={1750}
           height={375}
           className="block h-10 w-auto dark:hidden sm:h-14"
         />
         <img
-          src="/assets/gouache-logo-dark.png?v=1"
+          src="/assets/gouache-logo-dark.png?v=2"
           alt="Gouache"
           width={1750}
           height={375}
