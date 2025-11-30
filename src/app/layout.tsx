80b35f9e7f046c5a1e075d5319ae198c5bc9fdc0
Harry Rollerson <hrollerson1@gmail.com>
2025-11-30 12:18:55 +0800

    Update favicon to favicon.ico and update layout references

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export const metadata: Metadata = {
   description:
     'A social marketplace for artists and art lovers to connect, discover, and trade art.',
   icons: {
-    icon: '/gouache-favicon-20241119.ico?v=1',
-    shortcut: '/gouache-favicon-20241119.ico?v=1',
-    apple: '/gouache-favicon-20241119.ico?v=1',
+    icon: '/favicon.ico?v=1',
+    shortcut: '/favicon.ico?v=1',
+    apple: '/favicon.ico?v=1',
   },
 };
 
